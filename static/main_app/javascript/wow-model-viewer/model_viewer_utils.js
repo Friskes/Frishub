f0d7cafda30f2af300497a44912528bafab05786
Friskes <friskestv@gmail.com>
2023-06-21 15:27:56 +0300

    big refactoring dressing room and add touch events for mobile devices

--- static/main_app/javascript/wow-model-viewer/model_viewer_utils.js
+++ static/main_app/javascript/wow-model-viewer/model_viewer_utils.js
@@ -2,7 +2,9 @@ window.WH.debug = function(...args) {
     // console.log(args);
 };
 
-let CONTENT_PATH;
+const CONTENT_PATH = window.GAME_PATCH
+? `https://wow.zamimg.com/modelviewer/${window.GAME_PATCH}/`
+: 'https://wow.zamimg.com/modelviewer/wrath/';
 
 const RACES = {
     1: 'human',
@@ -17,77 +19,72 @@ const RACES = {
     11: 'draenei'
 };
 
-const CHARACTER_PART = {
-    // WRATH AND LIVE
-    'Skin Color':  'skin',
-    'Face':        'face',
-    'Hair Style':  'hairStyle',
-    'Horn Style':  'hairStyle',
-    'Horn Color':  'hairColor',
-    'Hair Color':  'hairColor',
-    'Horn Style':  'facialStyle',
-    'Hair':        'facialStyle',
-    'Facial Hair': 'facialStyle',
-    'Piercings':   'facialStyle',
-    'Markings':    'facialStyle',
-    'Features':    'facialStyle',
-    'Earrings':    'facialStyle',
-    'Tusks':       'facialStyle',
-
-    // ONLY LIVE
-    'Beard':          'facialStyle',
-    'Jaw Features':   'facialStyle',
-    'Markings Color': 'facialStyle',
-    'Horns':          'facialStyle',
-
-    // Не сделано
-    'Eyebrows':         undefined,
-    'Face Shape':       undefined,
-    'Eye Color':        undefined,
-    'Makeup':           undefined,
-    'Necklace':         undefined,
-    'Eyesight':         undefined,
-    'Scars':            undefined,
-    'Nose Ring':        undefined,
-    'Tattoo':           undefined,
-    'War Paint':        undefined,
-    'War Paint Color':  undefined,
-    'Tattoo Color':     undefined,
-    'Vines':            undefined,
-    'Vine Color':       undefined,
-    'Ears':             undefined,
-    'Blindfold':        undefined,
-    'Headdress':        undefined,
-    'Bear Form':        undefined,
-    'Cat Form':         undefined,
-    'Aquatic Form':     undefined,
-    'Travel Form':      undefined,
-    'Flight Form':      undefined,
-    'Moonkin Form':     undefined,
-    'Skin Type':        undefined,
-    'Face Features':    undefined,
-    'Face Paint':       undefined,
-    'Jewelry Color':    undefined,
-    'Flower':           undefined,
-    'Body Paint':       undefined,
-    'Paint Color':      undefined,
-    'Earring Color':    undefined,
-    'Face Paint Color': undefined,
-    'Piercing':         undefined,
-    'Body Paint Color': undefined,
-    'Armbands':         undefined,
-    'Bracelets':        undefined,
-    'Circlet':          undefined,
-    'Hair Decoration':  undefined,
-    'Tendrils':         undefined,
-    'Tail':             undefined,
-    'Mustache':         undefined,
-    'Grime':            undefined,
-    'Upright':          undefined,
-    'Sideburns':        undefined,
-    'Foremane':         undefined,
-    'Horn Decoration':  undefined
+const AVAILABLE_CHAR_OPTS = {
+    'Jaw Features':     'jaw_features',
+    'Hair Decoration':  'hair_decoration',
+    'Sideburns':        'sideburns',
+    'Beard':            'beard',
+    'Earrings':         'earrings',
+    'Circlet':          'circlet',
+    'Tusks':            'tusks',
+    'Armbands':         'armbands',
+    'Skin Color':       'skin_color',
+    'Markings Color':   'markings_color',
+    'Blindfold':        'blindfold',
+    'Eye Color':        'eye_color',
+    'Body Paint':       'body_paint',
+    'Foremane':         'foremane',
+    'Features':         'features',
+    'Tattoo':           'tattoo',
+    'Headdress':        'headdress',
+    'War Paint':        'war_paint',
+    'Ears':             'ears',
+    'Aquatic Form':     'aquatic_form',
+    'Earring Color':    'earring_color',
+    'Piercing':         'piercing',
+    'Scars':            'scars',
+    'Horns':            'horns',
+    'Upright':          'upright',
+    'Grime':            'grime',
+    'Horn Color':       'horn_color',
+    'Hair Style':       'hair_style',
+    'Markings':         'markings',
+    'Eyebrows':         'eyebrows',
+    'Body Paint Color': 'body_paint_color',
+    'Face Paint':       'face_paint',
+    'Vines':            'vines',
+    'Tail':             'tail',
+    'Facial Hair':      'facial_hair',
+    'Flight Form':      'flight_form',
+    'Face Paint Color': 'face_paint_color',
+    'Makeup':           'makeup',
+    'Face Features':    'face_features',
+    'Cat Form':         'cat_form',
+    'Horn Style':       'horn_style',
+    'Piercings':        'piercings',
+    'Necklace':         'necklace',
+    'Moonkin Form':     'moonkin_form',
+    'Mustache':         'mustache',
+    'Hair Color':       'hair_color',
+    'Flower':           'flower',
+    'Vine Color':       'vine_color',
+    'Tendrils':         'tendrils',
+    'Face':             'face',
+    'Jewelry Color':    'jewelry_color',
+    'Nose Ring':        'nose_ring',
+    'Bear Form':        'bear_form',
+    'Skin Type':        'skin_type',
+    'Travel Form':      'travel_form',
+    'Face Shape':       'face_shape',
+    'Eyesight':         'eyesight',
+    'Tattoo Color':     'tattoo_color',
+    'War Paint Color':  'war_paint_color',
+    'Horn Decoration':  'horn_decoration',
+    'Bracelets':        'bracelets',
+    'Paint Color':      'paint_color',
+    'Hair':             'hair'
 };
+
 
 /**
  * Возвращает двумерный список, внутренний список содержит в первой позиции - slotId,
@@ -96,26 +93,21 @@ const CHARACTER_PART = {
  * @returns {Promise<number[]>}
  */
 async function findItemsInEquipments(equipments) {
+
     for (const equipment of equipments) {
 
         const displayedItem = (Object.keys(equipment.transmog).length !== 0) ? equipment.transmog : equipment.item;
-        const displaySlot = await getDisplaySlot(
-            displayedItem.entry,
-            equipment.slot,
-            displayedItem.displayid
-        );
+
+        const displaySlot = await getDisplaySlot(displayedItem.entry, equipment.slot, displayedItem.displayid);
+
         equipment.displaySlot = displaySlot.displaySlot;
         equipment.displayId = displaySlot.displayId;
+
         Object.assign(displaySlot, equipment);
-    }
-    return equipments
-        .filter(e => e.displaySlot)
-        .map(e => [
-            e.displaySlot,
-            e.displayId
-        ]
-        );
-}
+    };
+    return equipments.filter(e => e.displaySlot).map(e => [e.displaySlot, e.displayId]);
+};
+
 
 /**
  *
@@ -124,16 +116,12 @@ async function findItemsInEquipments(equipments) {
  * @returns {Promise<Object>}
  */
 async function findRaceGenderOptions(race, gender) {
-    const options = await fetch(`${CONTENT_PATH}meta/charactercustomization2/${race}_${gender}.json`)
-        .then(
-            (response) => response.json()
-        );
-    if (options.data) {
-        return options.data;
-    }
+    const options = await fetch(`${CONTENT_PATH}meta/charactercustomization2/${race}_${gender}.json`).then((response) => response.json());
 
+    if (options.data) return options.data;
     return options;
-}
+};
+
 
 /**
  *
@@ -143,46 +131,29 @@ async function findRaceGenderOptions(race, gender) {
  * @return {Promise<boolean|*>}
  */
 async function getDisplaySlot(item, slot, displayId) {
-    if (typeof item !== `number`) {
-        throw new Error(`item must be a number`);
-    }
-
-    if (typeof slot !== `number`) {
-        throw new Error(`slot must be a number`);
-    }
-
-    if (typeof displayId !== `number`) {
-        throw new Error(`displayId must be a number`);
-    }
+    if (typeof item !== `number`) throw new Error(`item must be a number`);
+    if (typeof slot !== `number`) throw new Error(`slot must be a number`);
+    if (typeof displayId !== `number`) throw new Error(`displayId must be a number`);
 
     try {
         await fetch(`${CONTENT_PATH}meta/armor/${slot}/${displayId}.json`).then(response => response.json());
 
-        return {
-            displaySlot: slot,
-            displayId: displayId
-        };
+        return {displaySlot: slot, displayId: displayId};
     } catch (e) {
-        let contentPath = CONTENT_PATH.split('/').at(-2) == 'wrath'
+        const contentPath = CONTENT_PATH.split('/').at(-2) == 'wrath'
         ? 'https://wow.zamimg.com/modelviewer/live/'
         : 'https://wow.zamimg.com/modelviewer/wrath/';
-        await fetch(`${contentPath}meta/armor/${slot}/${displayId}.json`).then(response => response.json());
 
-        return {
-            displaySlot: slot,
-            displayId: displayId
+        try {
+            await fetch(`${contentPath}meta/armor/${slot}/${displayId}.json`).then(response => response.json());
+
+            return {displaySlot: slot, displayId: displayId};
+        } catch (e) {
+            console.error('[async function getDisplaySlot] -> await fetch -> *FAILED*');
         };
+    };
+};
 
-        // const resp = await fetch(`https://wotlk.murlocvillage.com/api/items/${item}/${displayId}`).then((response) => response.json());
-        // const res = resp.data ? resp.data : resp;
-        // if (res.newDisplayId !== displayId) {
-        //     return {
-        //         displaySlot: slot,
-        //         displayId: res.newDisplayId
-        //     };
-        // }
-    }
-}
 
 /**
  *
@@ -194,20 +165,19 @@ async function optionsFromModel(model) {
         // NPC or item
         const {id, type} = model;
         return {models: {id, type}};
-    }
+    };
 
     const {race, gender} = model;
 
     // CHARACTER OPTIONS
-    const fullOptions = await findRaceGenderOptions(
-        race,
-        gender
-    );
+    const fullOptions = await findRaceGenderOptions(race, gender);
 
     const characterItems = (model.items) ? model.items : [];
+
     const options = getOptions(model, fullOptions);
 
     const retGender = (gender === 1) ? `female` : `male`;
+
     const raceToModelId = RACES[race] + retGender;
 
     return {
@@ -218,9 +188,10 @@ async function optionsFromModel(model) {
         models: {
             id: raceToModelId,
             type: 16
-        },
+        }
     };
-}
+};
+
 
 /**
  *
@@ -230,9 +201,11 @@ async function optionsFromModel(model) {
  * @returns {Promise<ZamModelViewer>}
  */
 async function generateModels(aspect, containerSelector, model) {
-    CONTENT_PATH = model.general_opts.contentPath;
+
+    if (!model.general_opts || Object.keys(model.general_opts).length === 0) model.general_opts = {contentPath: CONTENT_PATH};
 
     const modelOptions = await optionsFromModel(model);
+
     const models = {
         type: 2,
         container: jQuery(containerSelector),
@@ -241,14 +214,17 @@ async function generateModels(aspect, containerSelector, model) {
         ...model.general_opts,
         ...modelOptions
     };
+
     window.models = models;
 
     return new ZamModelViewer(models);
-}
+};
+
 
 function optionalChaining(choice) {
     return choice ? choice.Id : undefined;
-}
+};
+
 
 /**
  *
@@ -257,43 +233,32 @@ function optionalChaining(choice) {
  * @return {Promise<[]>}
  */
 function getOptions(character, fullOptions) {
-    const options = fullOptions.Options;
 
-    // Для удобного просмотра поля Name из options
+    // Для удобного просмотра поля Name из fullOptions
     // const GENDERS = {0: 'Male', 1: 'Female'};
     // let dct = {};
-    // dct[`${RACES[character.race]}_${GENDERS[character.gender]}`] = [...new Set(options.map(opt => opt.Name))];
+    // dct[`${RACES[character.race]}_${GENDERS[character.gender]}`] = [...new Set(fullOptions.Options.map(opt => opt.Name))];
     // console.log(dct);
 
     const ret = [];
-    for (const prop in CHARACTER_PART) {
+    for (const prop in AVAILABLE_CHAR_OPTS) {
 
-        let CHAR_PROP = CHARACTER_PART[prop];
+        const part = fullOptions.Options.find(e => e.Name === prop);
 
-        // пропускаем конфликт рогов Horns с Night and Blood эльфами
-        if (prop == 'Horns') { if (character.race == 4 || character.race == 10) { continue; }; };
-
-        // изменяем 'facialStyle' на 'hairStyle' для Tauren для исключения конфликта настроек между Tauren и Draenei
-        if (character.race == 6 && prop == 'Horn Style' && CHAR_PROP == 'facialStyle') { CHAR_PROP = 'hairStyle'; };
-
-        const part = options.find(e => e.Name === prop);
-
-        if (!part) {
-            continue;
-        }
+        if (!part) continue;
 
         const newOption = {
             optionId: part.Id,
-            choiceId: (CHAR_PROP) ? optionalChaining(part.Choices[character[CHAR_PROP]]) : part.Choices[0].Id
+            choiceId: (AVAILABLE_CHAR_OPTS[prop]) ? optionalChaining(part.Choices[character[AVAILABLE_CHAR_OPTS[prop]]]) : part.Choices[0].Id
         };
         ret.push(newOption);
-    }
-
+    };
     return ret;
-}
+};
+
 
 export {
     findRaceGenderOptions,
     generateModels,
-    optionsFromModel,
+    optionsFromModel
 };
