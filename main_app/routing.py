ccf9897b2a8e146f6009fd2ceb37bea87ec385fc
Friskes <friskestv@gmail.com>
2023-08-11 13:43:31 +0300

    fix ws bug

--- main_app/routing.py
+++ main_app/routing.py
@@ -9,6 +9,8 @@ websocket_urlpatterns = [
     # префикс так же должен быть указан в javascript
     # он необходим для того чтобы прокси сервер nginx смог отличить
     # http/https от ws/wss протокола
+    # так же в javascript необходимо указывать порт daphne после хоста
+    # на продакшене (возможно при деплое с помощью docker порт добавлять не надо)
     re_path('ws/game-chat/', consumers.GameChatConsumer.as_asgi()),
 
     re_path(r'^ws/dev-chat/(?P<room_id>[^/]+)/$', consumers.DevChatConsumer.as_asgi()),
